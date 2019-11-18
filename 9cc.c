960e1235264132f328cac7d1630ccbcf45acf76f
Suisuikitai <mag.cupw@gmail.com>
2019-11-19 06:15:24 +0900

    tmp

--- 9cc.c
+++ 9cc.c
@@ -82,6 +82,93 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
   return tok;
 }
 
+// 要素は数と記号 + - * / ( ) から作られる
+// ( expr )は木の末端に配置し順序を保ってアセンブラを出力できれば良いので
+// ()自体を気に保持し、アセンブラに出力しない(というかアセンブラが()に対応しない)
+typedef enum {
+  ND_ADD,
+  ND_SUB,
+  ND_MUL,
+  ND_DIV,
+  ND_NUM,
+} NodeKind;
+
+//木構造を定義
+// 木構造はノードそれ自体の情報とそのノードが持つ参照がある
+// ノードそれ自体の情報: それが記号なのか、数なのかの種類　数値
+// -> NodeKind and Value
+// Referece: left and right (left-handle side, right-handle side)左辺と右辺
+// -> 
+typedef struct Node Node;
+
+struct Node{
+  NodeKind kind; // Indicate +,-,*,/ or number
+  int val;            // Hold number of Node if NodeKind is ND_NUM. If not, it has nothing.
+  Node *lhs;          // Reference left-handle side
+  Node *rhs;          // Reference right-handle side
+};
+
+// Append the Node. 
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs){
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+//If the NodeKind is ND_NUM, it has its value. this is for ND_NUM
+Node *new_node_num(int val){
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+// consume tokens then create syntax tree
+Node *expr();
+Node *mul();
+Node *primary();
+
+// it must follow EBNF expr
+// expr = mul("+" mul | "-" mul)*
+Node *expr(){
+  Node *node = mul();
+
+  for(;;) {
+    if (consume('+'))
+      node = new_node(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_node(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+//mul = primary("*" primary | "/" primary)*
+Node *mul(){
+  Node *node = primary();
+
+  for(;;){
+    if (consume('*'))
+      node = new_node(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_node(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+//primary = num | "(" expr ")"
+Node *primary(){
+  if (consume('(')){
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+  return new_node_num(expect_number());
+}
+
 // 入力文字列pをトークナイズしてそれを返す
 Token *tokenize() {
   char *p = user_input;
@@ -98,7 +185,6 @@ Token *tokenize() {
 
     if (*p == '+' || *p == '-') {
       cur = new_token(TK_RESERVED, cur, p++);
-      //インクリメントp++は関数が終了した時にインクリメントされる
       continue;
     }
 
@@ -130,7 +216,7 @@ int main(int argc, char **argv) {
   printf(".global main\n");
   printf("main:\n");
 
-  // 式の最初は数でなければならないので、それをチェックして
+  // 式の最初は数でなければならないので(そうでもない 例えば-4-2とかありえる)、それをチェックして
   // 最初のmov命令を出力
   printf("  mov rax, %d\n", expect_number());
 
